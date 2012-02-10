d77bb2ce976705881a93d83a46aa660725648e00
Christian Kniep <kniep@videobeatz.de>
2012-02-10 21:29:41 +0100

    init-postgres file

---
+++ serverfiles/usr/local/postgres/init_pg.sql
@@ -0,0 +1,22 @@
+SET client_encoding = 'UTF8';
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+DROP DATABASE "qnib";
+
+CREATE DATABASE "qnib" WITH TEMPLATE = template0 ENCODING = 'UTF8';
+
+
+ALTER DATABASE "qnib" OWNER TO postgres;
+
+\connect "qnib"
+
+
+COMMENT ON SCHEMA public IS 'Standard public schema';
+
+CREATE LANGUAGE plpgsql;
+
+\i tables.sql;
+\i init_tabs.sql;
