324e359bcbd2a84a0886d97ad325f3018eb97c0d
Christian Kniep <christian@qnib.org>
2012-03-14 15:47:59 +0100

    nodetype root is added when initilize the db

--- serverfiles/usr/local/postgres/init_tabs.sql
+++ serverfiles/usr/local/postgres/init_tabs.sql
@@ -1,6 +1,7 @@
 /* Nodetypes */
 insert into nodetypes (nt_name) VALUES ('host');
 insert into nodetypes (nt_name) VALUES ('switch');
+insert into nodetypes (nt_name) VALUES ('root');
 
 /* Chassis */
 insert into chassis (c_id,c_name) VALUES ('0','empty');
